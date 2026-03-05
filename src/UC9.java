c0e11460b14427442564d3bef5520482e2fb9f1f
akshadhas01 <akshadhas01@gmail.com>
2026-03-05 11:46:53 +0530

    UC9: Recursive Palindrome Checker

---
+++ src/UC9.java
@@ -0,0 +1,24 @@
+public class UC9 {
+
+    private static boolean check(String s, int start, int end) {
+
+        if (start >= end) {
+            return true;
+        }
+
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        return check(s, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome?");
+        System.out.println(check(input, 0, input.length() - 1));
+    }
+}
